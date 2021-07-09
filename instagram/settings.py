c9953e1dc0af74f68949cfec5b30e501a237b535
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-09 20:22:05 +0300

    connect the database to the project

--- instagram/settings.py
+++ instagram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gram.apps.GramConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gram',
+        'USER': 'wangari',
+    'PASSWORD':'nyeri12032',
+    
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
