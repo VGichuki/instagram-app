6e37961f8ac448d174658470fe426f47ab95d6ad
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-11 18:35:48 +0300

    create a route for profile and search function

--- gram/urls.py
+++ gram/urls.py
@@ -8,7 +8,9 @@ urlpatterns=[
     url('new/image/', views.upload_image, name = 'new-image'),
     url('register/', views.registerPage, name = 'register'),
     url('login/', views.loginPage, name = 'login'),
-    url('logout/', views.logoutUser, name = 'logout')
+    url('logout/', views.logoutUser, name = 'logout'),
+    url('profile/',views.profile, name= 'profile'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
