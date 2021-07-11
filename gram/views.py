a08616fb8957978da23fd477cf41f75baa6652b9
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-11 12:23:17 +0300

    create a login.html and when a user logs in they are redirected to the home page

--- gram/views.py
+++ gram/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import Profile,Post,Comment,Follow
 from .forms import PostForm, CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def index(request):
@@ -29,9 +31,25 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get("username")
+            messages.success(request, 'Account was created for ' + user)
+
+            return redirect('login')
 
     context = {"form" : form}
     return render(request, 'registration/register.html', context)
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+    context = {}
+    return render(request, 'registration/login.html')
 
 
 
